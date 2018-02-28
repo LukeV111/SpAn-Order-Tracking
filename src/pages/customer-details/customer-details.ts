be164aff3d76d5553f5e44970478d3a32a6414bf
LukeV111 <lukegventer@gmail.com>
2018-02-28 10:44:18 +0200

    Luke Commit

--- src/pages/customer-details/customer-details.ts
+++ src/pages/customer-details/customer-details.ts
@@ -1,8 +1,9 @@
+
 import { Component } from '@angular/core';
-import { NavController, NavParams, ToastController} from 'ionic-angular';
+import { NavController, NavParams, ToastController } from 'ionic-angular';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { FirebaseService } from './../../providers/firebase-service';
-import { FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2/database';
+import { FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
 import { AlertController } from 'ionic-angular';
 import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -10,8 +11,8 @@ import { EditCustomerPage } from '../edit-customer/edit-customer';
 import firebase from 'firebase';
 
 @Component({
-  selector: 'page-customer-details',
-  templateUrl: 'customer-details.html',
+	selector: 'page-customer-details',
+	templateUrl: 'customer-details.html',
 })
 export class CustomerDetailsPage {
 
@@ -20,44 +21,42 @@ export class CustomerDetailsPage {
 	public customerArray; any = [];
 	public profileForm: FormGroup;
 	public profile: firebase.database.Reference;
-	public customerRef: firebase.database.Reference; 
+	public customerRef: firebase.database.Reference;
 
 
 	constructor(private db: AngularFireDatabase, public fb: FormBuilder, public navCtrl: NavController, private toastCtrl: ToastController, public alertCtrl: AlertController, public firebaseService: FirebaseService, public navParams: NavParams, private auth: AuthServiceProvider) {
 
-				this.authUser = this.auth.getLoggedInUser();
-			  if (this.authUser) {
-					this.customer = this.navParams.data; //This line is referened by the html. 
-					this.customerRef = firebase.database().ref('/users/' + this.authUser.uid + '/Customers/');//This line is also referened by the html.
-					//this.db.list('/users/' + this.authUser.uid + '/Customers/').subscribe(items => {
-				  //}); //This doesn't seem to do anything.
-					//this.customerRef = firebase.database().ref('/users/' + this.authUser.uid + '/Customers/') //This is where you need to get that key.
-					console.log(this.customerRef.key)	
-					//console.log('this.customer.key')		
-	
-			  };
+		this.authUser = this.auth.getLoggedInUser();
+		if (this.authUser) {
+			this.customer = this.navParams.data; //This line is referened by the html. 
+			this.customerRef = firebase.database().ref('/users/' + this.authUser.uid + '/Customers/' + this.customer.$key);//This line is also referened by the html.
+			this.db.list('/users/' + this.authUser.uid + '/Customers/').subscribe(items => {}); //This doesn't seem to do anything.
+			console.log(this.customer.$key)
+			//console.log('this.customer.key')		
 
-			  this.profileForm = fb.group({
-				  'companyName': [''],
-					'customerName': [''],
-					'accountsManager': [''],
-					'accountsEmail': [''],
-				  'customerEmail': [''],
-				  'customerCode': [''],
-				  'customerTracking': [''],
-					'grindType': [''],
-					'location': [''],
-					'paymentTerms': [''],
-					'notes': [''],
-			  });    
-			}
+		};
+
+		this.profileForm = fb.group({
+			'companyName': [''],
+			'customerName': [''],
+			'accountsManager': [''],
+			'accountsEmail': [''],
+			'customerEmail': [''],
+			'customerCode': [''],
+			'customerTracking': [''],
+			'grindType': [''],
+			'location': [''],
+			'paymentTerms': [''],
+			'notes': [''],
+		});
+	}
 
 	saveTracking(customer) {
 		const updateTracking = this.db.list('/users/' + this.authUser.uid + '/Customers/');
 		updateTracking.update(customer.$key, customer).then(() => {
 			console.log("Saved");
 		});
-}
+	}
 
 	presentToast(position: string, message: string) {
 		let toast = this.toastCtrl.create({
@@ -66,18 +65,13 @@ export class CustomerDetailsPage {
 			duration: 3000
 		});
 		toast.present();
-	}4
+	} 4
 
 	updateProfile(customerRef) {
-		const updateProfile = this.db.list('/users/' + this.authUser.uid + '/Customers/');
-		customerRef.update(this.profileForm.value).then(() => {
-      this.presentToast('middle', 'Customer Profile is updated');
-    });
+		this.customerRef.update(this.profileForm.value).then(() => {
+			this.presentToast('middle', 'Customer Profile is updated');
+		});
 	}
 
 
 }
-
-
-
-////
